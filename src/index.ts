4b73fa7fc6cc61dd3364ebc37e78d1ba76290916
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:56 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export type {
 } from './types';
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
-export { luggageCalculator, LUGGAGE_CALCULATOR_TOOL, LuggageCalculator, LuggageCalculatorSEO, LuggageCalculatorBibliography } from './tool/luggage-calculator';
-export { tipCalculator, TIP_CALCULATOR_TOOL, TipCalculator, TipCalculatorSEO, TipCalculatorBibliography } from './tool/tip-calculator';
-export { suitcaseChecklist, SUITCASE_CHECKLIST_TOOL, SuitcaseChecklist, SuitcaseChecklistSEO, SuitcaseChecklistBibliography } from './tool/suitcase-checklist';
-export { miniAdventures, MINI_ADVENTURES_TOOL, MiniAdventures, MiniAdventuresSEO, MiniAdventuresBibliography } from './tool/mini-adventures';
+export { luggageCalculator, LUGGAGE_CALCULATOR_TOOL, LuggageCalculator } from './tool/luggage-calculator';
+export { tipCalculator, TIP_CALCULATOR_TOOL, TipCalculator } from './tool/tip-calculator';
+export { suitcaseChecklist, SUITCASE_CHECKLIST_TOOL, SuitcaseChecklist } from './tool/suitcase-checklist';
+export { miniAdventures, MINI_ADVENTURES_TOOL, MiniAdventures } from './tool/mini-adventures';
